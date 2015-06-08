c860f02fdd300e1a143771b9dd291a5214c1e4a5
Steven Bassett <steven.j.bassett@gmail.com>
2015-06-07 17:51:48 -0700

    Adding nicknames to the data

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 (function (window) {
   'use strict'
 
-  function CatModel (name, imgSrc, imgAttr) {
+  function CatModel (name, imgSrc, imgAttr, nickNames) {
     this.name = ko.observable(name)
     this.clicks = ko.observable(0)
     this.imgSrc = ko.observable(imgSrc)
@@ -11,20 +11,43 @@
     this.imgAlt = ko.computed(function () {
       return 'picture of ' + this.name()
     }, this)
+    this.level = ko.computed(function () {
+      var clicks = this.clicks()
+      if (clicks > 101) {
+        return 'Senior'
+      } else if (clicks > 41) {
+        return 'Over-The-Hill'
+      } else if (clicks > 31) {
+        return 'Adult'
+      } else if (clicks > 21) {
+        return 'Young Adult'
+      } else if (clicks > 11) {
+        return 'Teen'
+      } else if (clicks > 3) {
+        return 'Kitten'
+      } else {
+        return 'Infant'
+      }
+    }, this)
+    this.nickNames = ko.observableArray(nickNames)
     this.incrementClicks = function () {
       this.clicks(this.clicks() + 1)
     }.bind(this)
   }
 
   function CatCollection (cats) {
+    var self = this;
     this.cats = ko.observableArray(cats.map(function (cat) {
-      return new CatModel(cat.name, cat.imgSrc, cat.imgAttr)
+      return new CatModel(cat.name, cat.imgSrc, cat.imgAttr, cat.nickNames)
     }))
     var first = this.cats()[0]
     this.selected = ko.observable(first)
     this.select = function (cat) {
       this.selected(cat)
     }.bind(this)
+    this.isSelected = function (index) {
+      return self.cats.indexOf(self.selected()) === index()
+    }
   }
 
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var chance = new Chance()
 var app = jsonServer.create()
 
 var data = {
-  cats: generateData()
+  cats: generateData(1000)
 }
 
 var router = jsonServer.router(data)
@@ -21,13 +21,24 @@ function generateData (number) {
   number = number || 100
   var cats = []
   var w, h
+
+  function getNickNames () {
+    var nickNames = []
+    var num = chance.natural({min: 0, max: 10})
+    for (var i = 0; i < num; i++) {
+      nickNames.push(chance.first())
+    }
+    return nickNames
+  }
   for (var i = 0; i < number; i++) {
     w = chance.natural({min: 200, max: 300})
     h = chance.natural({min: 200, max: 300})
+
     cats.push({
       name: chance.name(),
       imgSrc: 'https://placekitten.com/g/' + w + '/' + h,
-      imgAttr: 'https://placekitten.com/attribution.html'
+      imgAttr: 'https://placekitten.com/attribution.html',
+      nickNames: getNickNames()
     })
   }
   return cats
